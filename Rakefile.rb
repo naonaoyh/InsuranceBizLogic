6d6c6d48b8f3d6580b1eece6210639d6f7519548
mawdo <monkeypatch@gmail.com>
2009-06-19 00:03:02 +0100

    remove hard coded path to IABDB

--- Rakefile.rb
+++ Rakefile.rb
@@ -3,10 +3,6 @@ require 'rake'
 require 'rake/testtask'
 require 'rake/runtest'
 
-task :default => "pkg/#{spec.name}-#{spec.version}.gem" do
-    puts "generated latest version because of the dependency"
-end
-
 task :test do
   Rake.run_tests '**/*Test.rb'
 end
@@ -15,10 +11,6 @@ task :vtest do
   Rake.run_tests '**/VPMSCallTest.rb'
 end
 
-task :reinstall => [:default] do
-  system "gem uninstall #{spec.name}"
-  system "gem install pkg/#{spec.name}-#{spec.version}"
-end
 
 # This is what the task should look like to run the unit tests
 #task :cruise => [:test, :reinstall]
